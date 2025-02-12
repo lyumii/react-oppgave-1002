be91cf5c4cccb887b8d233d026690f0a0249af5d
lyumii <dmly1124@gmail.com>
2025-02-12 14:50:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import Clicker from "./Components/Clicker";
+
+function App() {
+  return <Clicker />;
+}
+export default App;
---
+++ src/Components/Clicker.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import badcookieimg from "../assets/badcookie.png";
+import cookieimg from "../assets/cookie.png";
+import goldencookieimg from "../assets/goldencookie.png";
+
+export default function Clicker() {
+  const [click, setClicker] = React.useState(0);
+  const [cookieState, setCookieState] = React.useState(cookieimg);
+
+  const clickCookie = () => {
+    let cookieNum = Math.floor(Math.random() * 10) + 1;
+    if (cookieNum === 1) {
+      setCookieState(badcookieimg);
+      setClicker((prevClick) => {
+        if (prevClick <= 0) {
+          return 0;
+        } else {
+          return prevClick - 2;
+        }
+      });
+    } else if (cookieNum === 10) {
+      setCookieState(goldencookieimg);
+      setClicker((prevClick) => prevClick + 3);
+    } else {
+      setCookieState(cookieimg);
+      setClicker((prevClick) => prevClick + 1);
+    }
+  };
+
+  return (
+    <>
+      <h2>Your neighbourhood friendly Cookie Clicker, but with a twist!</h2>
+      <p> Pointless amount of points: {click} </p>
+      <div onClick={clickCookie}>
+        <img src={cookieState} alt="Cookie" />
+        <p>
+          {cookieState === cookieimg
+            ? "Tasty tasty cookie!"
+            : cookieState === badcookieimg
+            ? "What a bad cookie!"
+            : "Woowwie! A golden cookie!"}
+        </p>
+      </div>
+      <button onClick={() => setClicker(() => 0)}>Clear Cookies :3</button>
+    </>
+  );
+}
