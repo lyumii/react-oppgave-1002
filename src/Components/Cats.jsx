e0dd87cc9ed4a185dfbd6fbb78a823da68974869
lyumii <dmly1124@gmail.com>
2025-02-13 14:37:01 +0100

    more cat flavor

--- src/Components/Cats.jsx
+++ src/Components/Cats.jsx
@@ -4,9 +4,22 @@ export default function Cats() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  async function fetchAll() {
+    setLoading(true);
+    try {
+      const promise1 = catfacts();
+      const promise2 = catpics();
+      const result = await Promise.all([promise1, promise2]);
+      return result;
+    } catch (error) {
+      setError("No fact for you, sorry");
+    } finally {
+      setLoading(false);
+    }
+  }
+
   async function catfacts() {
     const url = "https://catfact.ninja/fact?max_length=140";
-    setLoading(true);
 
     try {
       const response = await fetch(url);
@@ -17,9 +30,7 @@ export default function Cats() {
       console.log(data.fact);
       setFacts(data.fact);
     } catch (error) {
-      setError("No fact for you, sorry");
-    } finally {
-      setLoading(false);
+      console.log("Error", error);
     }
   }
 
@@ -42,31 +53,32 @@ export default function Cats() {
   const [catPic, setCatPic] = useState("");
 
   useEffect(() => {
-    catfacts();
-    catpics();
+    fetchAll();
   }, []);
 
   return (
     <section>
       <div className="catBtns">
         <div className="catDiv">
-          <img className="catimg" src={catPic} alt="picture of a cat" />
           {loading ? (
             <p className="catPar">...loading...</p>
           ) : error ? (
             <p className="catPar">{error}</p>
           ) : (
-            <p className="catPar">{facts}</p>
+            <article>
+              <img className="catimg" src={catPic} alt="picture of a cat" />
+              <p className="catPar">{facts}</p>
+            </article>
           )}
         </div>
         <div className="btnDiv">
-          <button>New fact please</button>
+          <button onClick={fetchAll}>New fact please</button>
           <button>Fave me please</button>
         </div>
       </div>
-      <article className="favelist">
+      {/* <article className="favelist">
         <h1 className="catlisth">My fave cat facts!</h1>
-      </article>
+      </article> */}
     </section>
   );
 }
